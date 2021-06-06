94a0456c957774a0780f463859f6e7baec30d43f
Ankur Sharma <ankur.101045@gmail.com>
2021-06-06 20:25:01 +0530

    added search keyword

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,13 @@ export class AppComponent implements OnInit {
   tabIndex: number;
   todaysTime;
   backgroundImg = "";
-  quoteObj = {'quote': '','author': ''};
-  jokeObj = {'joke': ''};
+  quoteObj = { 'quote': '', 'author': '' };
+  jokeObj = { 'joke': '' };
   showKeyNews = true
   showQuote = false
   showJoke = false
+  newsSrcBkp;
+  searchKey='';
   sourceMappings = {
     'all': 'International', 'general': 'India',
     'business': 'Business', 'entertainment': 'Entertainment',
@@ -34,7 +36,7 @@ export class AppComponent implements OnInit {
     this.http.get("https://blazingnews-api.herokuapp.com/all").subscribe((res: Array<object>) => {
       for (var elem of res) {
         elem['source'] = elem['title'].substring(elem['title'].indexOf('(s'));
-        elem['title'] = elem['title'].substring(0,elem['title'].indexOf('(s'));
+        elem['title'] = elem['title'].substring(0, elem['title'].indexOf('(s'));
         elem["show"] = false;
         if (elem["urlToImage"].includes("./img")) {
           let arr = elem["urlToImage"].split("/");
@@ -42,20 +44,22 @@ export class AppComponent implements OnInit {
         }
       }
       this.newsSource = res;
+      this.newsSrcBkp = res;
     })
-    this.http.get('https://blazingnews-api.herokuapp.com/backImg').subscribe(res=>{
+    this.http.get('https://blazingnews-api.herokuapp.com/backImg').subscribe(res => {
       this.backgroundImg = `https://www.bing.com${res['url']}`;
     })
     this.fetchKeyNews(0);
-    setInterval(()=>{
+    setInterval(() => {
       this.todaysTime = Date.now();
-    },10)
+    }, 10)
   }
 
   getNews(source) {
     this.showKeyNews = true;
     this.newsSource = null;
     this.loading = true;
+    this.searchKey = '';
     this.tabIndex = this.refMap[source];
     this.sourcePlaceholder = this.sourceMappings[source];
     this.source = source;
@@ -64,7 +68,7 @@ export class AppComponent implements OnInit {
     this.http.get("https://blazingnews-api.herokuapp.com/" + source).subscribe((res: Array<object>) => {
       for (var elem of res) {
         elem['source'] = elem['title'].substring(elem['title'].indexOf('(s'));
-        elem['title'] = elem['title'].substring(0,elem['title'].indexOf('(s'));
+        elem['title'] = elem['title'].substring(0, elem['title'].indexOf('(s'));
         elem["show"] = false;
         if (elem["urlToImage"].includes("./img")) {
           let arr = elem["urlToImage"].split("/");
@@ -72,6 +76,7 @@ export class AppComponent implements OnInit {
         }
       }
       this.newsSource = res;
+      this.newsSrcBkp = res;
       this.loading = false;
     })
   }
@@ -98,25 +103,31 @@ export class AppComponent implements OnInit {
     this.getNews(this.source);
   }
 
-  getQuote(){
+  getQuote() {
     this.showQuote = true
     this.showJoke = false
     this.showKeyNews = false;
     this.newsSource = null;
-    this.http.get('https://favqs.com/api/qotd').subscribe(data=>{
+    this.http.get('https://favqs.com/api/qotd').subscribe(data => {
       this.quoteObj.quote = data['quote']['body']
       this.quoteObj.author = data['quote']['author']
       console.log(this.quoteObj)
     })
   }
 
-  getJoke(){
+  getJoke() {
     this.showQuote = false
     this.showJoke = true
     this.showKeyNews = false
     this.newsSource = null
-    this.http.get('https://blazingnews-api.herokuapp.com/joke').subscribe(data=>{
-     this.jokeObj.joke = data['joke']
+    this.http.get('https://blazingnews-api.herokuapp.com/joke').subscribe(data => {
+      this.jokeObj.joke = data['joke']
     })
+  }
+  searchKeyword(event) {
+    let searchItem = event.target.value.toLowerCase();
+    this.newsSource = this.newsSrcBkp.filter((item=>{
+      return item.title.toLowerCase().includes(searchItem)
+    }))
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DeferLoadModule } from '@trademe/ng-defer-load';
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { DeferLoadModule } from '@trademe/ng-defer-load';
     BrowserModule,
     DeferLoadModule,
     HttpClientModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AnimateOnScrollModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
